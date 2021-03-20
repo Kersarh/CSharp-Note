0ed980ff56d469504bfcc5a2df38363b822c7e49
Kersarh <kers3412.git@gmail.com>
2021-03-21 01:12:41 +0300

    hello

---
+++ 1.Basic/01.Основы/01.HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; // Подключаемые модули (пространства имен)
+
+namespace MyProgram // Объявление нового пространства имен
+{ // {} открывают и закрывают блоки кода
+    class Program // Объявление класса
+    { 
+        static void Main() // Объявление метода Main, точка входа в программу.
+        {
+            Console.WriteLine("Hello World!"); // Пишем текст в консоль
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine(); // получаем консольный ввод и сохраняем в name
+            Console.WriteLine($"Привет {name}");
+        }
+    }
+}
