89f269335b121943879a36ab65ad9dc61920dd4d
Kersarh <kers3412.git@gmail.com>
2021-04-01 23:19:47 +0300

    ooп

---
+++ 1.Basic/02.ООП/03.1. Наследование/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            * Наследование позволяет создать новый класс на основе уже существующего(родительского) сохранив при этом свойства и функциональность.
+            */
+
+            // Создадим экземпляр класса B наследующего от класса A
+            B obj = new();
+            // В классе и мы можем использовать поля и методы класса A
+            obj.Method1();
+            // Так и дополнять класс новыми методами
+            obj.Method2();
+
+        }
+    }
+
+    class A
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Класс A, Метод 1");
+        }
+    }
+
+    class B : A // класс B наследует свойства от A
+    {
+        public void Method2()
+        {
+            Console.WriteLine("Класс B, Метод 2");
+        }
+    }
+
+}
