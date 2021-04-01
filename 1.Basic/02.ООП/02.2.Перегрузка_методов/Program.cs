97aa792eb3f04d0c7a75b05cdef86d77c92dac58
Kersarh <kers3412.git@gmail.com>
2021-04-01 18:42:27 +0300

    Method

---
+++ 1.Basic/02.ООП/02.2.Перегрузка_методов/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Перегрузка методов
+             * Допускается совместное использование одного и того же имени 
+             * двумя или более методами одного и того же класса.
+             * При этом их параметры должны быть различны. 
+             */
+
+            // Вызовем перегруженный метод sum с различными параметрами
+            Sum(); // Not Data
+            Sum(1); // 2
+            Sum(3, 7); // 10 
+
+        }
+
+        static void Sum()
+        {
+            Console.WriteLine("Not Data");
+        }
+
+        static void Sum(int x)
+        {
+            Console.WriteLine(x+1);
+        }
+
+        static void Sum(int x, int y) 
+        {
+            Console.WriteLine(x+y);
+        }
+    }
+}
