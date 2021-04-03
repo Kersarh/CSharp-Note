924669127200867a6d5f93cb4e8a42818282f6bc
Kersarh <kers3412.git@gmail.com>
2021-04-03 23:35:24 +0300

    delegate

---
+++ 1.Basic/03.Делегаты/02.Анонимные методы/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        delegate void MessageHandler(string message);
+
+        static void Main(string[] args)
+        {
+            /* Анонимная функция — это "встроенный" оператор или выражение,
+            *  которое может использоваться, когда тип делегата неизвестен.
+            *  Ее можно использовать для инициализации именованного делегата
+            *  или передать вместо типа именованного делегата в качестве параметра метода.
+            */
+
+            // Определение как функцию
+            MessageHandler handler = delegate (string mes)
+            {
+                Console.WriteLine(mes);
+            };
+
+            handler("hello world!");
+
+            // Определение через лямбда функцию
+            MessageHandler handler2 = (x) => { Console.WriteLine(x); };
+            handler2("Привет Мир!!!");
+
+        }
+    }
+}
