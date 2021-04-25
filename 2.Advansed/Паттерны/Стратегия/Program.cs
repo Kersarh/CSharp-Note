e5d973140282fec9b2e4a0ffadd4372f0f8b0945
Kersarh <kers3412.git@gmail.com>
2021-04-25 15:18:55 +0300

    strat

---
+++ 2.Advansed/Паттерны/Стратегия/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace MyProgram
+{
+    // Паттерн Стратегия(Strategy) определяет набор алгоритмов, инкапсулирует их и обеспечивает их взаимозаменяемость.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal auto = new(new FlyMove());
+            auto.MyMethod();
+            auto.SetStrategy(new RaceMove());
+            auto.MyMethod();
+        }
+    }
+
+    // Интерфейс используется для реализации алгоритма стратегии
+    interface IStrategy
+    {
+        void Move();
+    }
+
+    // Классы реализующие конкретные стратегии
+    class FlyMove : IStrategy
+    {
+        public void Move()
+        {
+            Console.WriteLine("По воздуху");
+        }
+    }
+
+    class RaceMove : IStrategy
+    {
+        public void Move()
+        {
+            Console.WriteLine("По земле");
+        }
+    }
+
+
+    class Animal
+    {
+        // Хранит ссылку на один из объектов Стратегии.
+        private IStrategy _mov;
+
+        // Принимает стратегию через конструктор
+        public Animal(IStrategy strategy)
+        {
+            _mov = strategy;
+        }
+
+        // Изменение стратегии во время выполнения
+        public void SetStrategy(IStrategy strategy)
+        {
+            _mov = strategy;
+        }
+
+        // Метод через который реализуется стратегия
+        public void MyMethod()
+        {
+            // Вызовет Move заданной стратегии
+            _mov.Move();
+        }
+    }
+}
