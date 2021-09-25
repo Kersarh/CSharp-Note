4c8ee57475cb1fac0e010e76935eac0b4697c35f
Kersarh <kers3412.git@gmail.com>
2021-09-25 16:44:59 +0300

    EditXML

---
+++ 1.Basic/10.XML/03. EditXML/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            ShortCreateXML();
+
+            XDocument xdoc = XDocument.Load("Users.xml");
+
+            XElement firstRunElem = xdoc.Element("Users");
+
+            foreach (XElement xe in firstRunElem.Elements("user").ToList())
+            {
+                // Если user имеет атрибут name = Alise меняем пароль
+                if (xe.Attribute("name").Value == "Alise")
+                {
+                    xe.Element("Authorization").Element("Password").Value = "New_Password";
+                }
+            }
+
+            // добавляем новый элемент
+            firstRunElem.Add(new XElement("user",
+                        new XAttribute("name", "Admin"),
+                    new XElement("Authorization",
+                        new XElement("Login", "admin_login"),
+                        new XElement("Password", "zxcvbn")),
+                    new XElement("ID", "0")));
+
+            // Сохраняем изменения в файле
+            xdoc.Save("Users.xml");
+
+            System.Diagnostics.Process.Start("notepad.exe", "Users.xml");
+        }
+
+        private static void ShortCreateXML()
+        {
+            XDocument xdoc = new(
+            new XElement("Users",
+                new XElement("user",
+                    new XAttribute("name", "Alise"),
+                    new XElement("Authorization",
+                        new XElement("Login", "Alise_login"),
+                        new XElement("Password", "qwerty")),
+                    new XElement("ID", "1")),
+                new XElement("user",
+                    new XAttribute("name", "Bob"),
+                    new XElement("Authorization",
+                        new XElement("Login", "Bob_login"),
+                        new XElement("Password", "asdfg")),
+                    new XElement("ID", "2"))));
+            xdoc.Save("Users.xml");
+        }
+    }
+}
