9c199e23a9d03d399b5c6e6dc73d1515e397dd5e
Kersarh <kers3412.git@gmail.com>
2021-09-30 21:00:46 +0300

    reflex

---
+++ 2.Advansed/Рефлексия/02.Связывание/MyDll/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyDll
+{
+    public class Class1
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public Class1()
+        {
+            Name = "user";
+            Password = "qwerty";
+        }
+
+        public string SendLoginPassword(string sep)
+        {
+            return Name + sep + Password;
+        }
+
+        static void MyStaticMethod()
+        {
+            Console.WriteLine("MyStaticMethod");
+        } 
+    }
+}
---
+++ 2.Advansed/Рефлексия/02.Связывание/Program/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Загружаем нашу DLL
+            Assembly asm = Assembly.LoadFrom("MyDll.dll");
+            Console.WriteLine(asm.FullName);
+
+            Type[] types = asm.GetTypes();
+            foreach (Type t1 in types)
+            {
+                Console.WriteLine(t1.Name);
+            }
+
+
+            // Позднее связывание
+
+            Type t = asm.GetType("MyDll.Class1", true, true);
+
+            // создаем экземпляр класса MyDll.Class1
+            object obj = Activator.CreateInstance(t);
+
+            // получаем метод SendLoginPassword
+            MethodInfo method = t.GetMethod("SendLoginPassword");
+
+            // вызываем метод с параметрами
+            object result = method.Invoke(obj, new object[] { " - " }); // method.Invoke(obj, null) -  если нет параметров
+            Console.WriteLine(result);
+
+        }
+    }
+}
