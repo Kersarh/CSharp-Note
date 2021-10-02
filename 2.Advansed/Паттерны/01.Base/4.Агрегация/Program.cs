ae8ad720fff1c270fa493cbacbe287e35f143868
Kersarh <kers3412.git@gmail.com>
2021-10-02 21:02:50 +0300

    agr

---
+++ 2.Advansed/Паттерны/01.Base/4.Агрегация/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyProgram
+{
+    // Агрегация – это когда экземпляр класса создается где-то в другом месте кода,
+    // и передается в конструктор в качестве параметра.
+    // При агрегации объекты Car и Engine будут равноправны.
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Engine eng = new();
+            Car car = new(eng);
+
+            car.engine.PrintType();
+        }
+    }
+
+    public class Engine
+    {
+        public void PrintType()
+        {
+            Console.WriteLine("Двигатель");
+        }
+    }
+
+    public class Car
+    {
+        public Engine engine;
+
+        public Car(Engine eng)
+        {
+            engine = eng;
+        }
+    }
+}
