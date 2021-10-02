e6cd40c32f8fbf9d331dbd68af7e1fc1151e2ec8
Kersarh <kers3412.git@gmail.com>
2021-10-02 20:13:27 +0300

    pat real

---
+++ 2.Advansed/Паттерны/01.Base/1.Реализация/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyProgram
+{
+    //Реализация основывается на определенном интерфейсе с последующей его реализации в классах.
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Animal an = new();
+            an.Move();
+        }
+    }
+
+    public interface IMovable // Интерфейс который мы будем реализовывать в классах
+    {
+        void Move();
+    }
+
+    public class Animal : IMovable // Класс реализующий интерфейс
+    {
+        public void Move()
+        {
+            Console.WriteLine("Бег");
+        }
+    }
+}
